ec1307e6df5b5c29b712ddce21383099f2367240
Namashivayan Sivaram <nsivaram@uwo.ca>
2023-03-12 20:10:28 -0400

    linux and windows functioanlity

--- Source.cpp
+++ Source.cpp
@@ -3,6 +3,9 @@
 * an app for printing all game results of 2022-2023 NHL regular season games
 * 2022-02-06
 */
+#if defined(_WIN32) || defined(WIN32) || defined(__CYGWIN__) || defined(__MINGW32__) || defined(__BORLANDC__)
+#define OS_WIN
+#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,8 +16,14 @@
 #include "GoogleAPI.h"
 #include <vector>
 #include "Location.h";
-//#include <unistd.h> //LINUX ONLY IF YOU ARE ON WINDOWS USE <windows.h> and change sleep to 10000
+#ifdef OS_WIN
 #include <Windows.h>
+#define SLEEP_TIME Sleep(10000)
+#else
+#include <unistd.h>
+#define SLEEP_TIME sleep(10)
+#endif
+
 using json = nlohmann::json;
 using namespace std;
 int main() {
@@ -36,7 +45,7 @@ int main() {
         std::cout << "Current location: " << myLoc.getLat()<< " : "<<myLoc.getLng() << std::endl;
 
 
-        Sleep(10000); // sleep for 10 seconds
+        SLEEP_TIME; // sleep for 10 seconds
     }
 
 

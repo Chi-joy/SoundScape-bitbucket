b4adb523b1f22ac44b2803936a69df120b80cb0a
Emily Chan <echan392@uwo.ca>
2023-04-04 01:19:17 +0000

    dox

--- shubh/SoundScape/playlist.h
+++ shubh/SoundScape/playlist.h
@@ -1,3 +1,12 @@
+/**
+ * @file playlist.h
+ * @brief Header file of the playlist class.
+ * 
+ * This file contains the declaration of the playlist class, which
+ * holds a playlist.
+ * 
+ * @author Shubh Fageria, Namashivayan Sivaram, Chi Zhang, Emily Chan, Valerie Lozano
+ */
 
 #include "Song.h"
 
@@ -12,22 +21,70 @@ namespace Playlist {
         QString playlistName;
         QString playlistURI;
         QString playlistCoverURI;
-        //std::vector<Song::song> sVector;
         bool hasLocation;
 
     public:
+      	/**
+ 		* @brief Constructs a new playlist object.
+ 		* 
+ 		* This constructor creates a new playlist object with no parameters.
+ 		* It is the default.
+ 		*/
         playlist();
+      
+      	/**
+ 		* @brief Constructs a new playlist object.
+ 		* 
+ 		* This constructor creates a new playlist object with the given song name, song URI, and song artist.
+ 		* 
+ 		* @param playlistName Name of the playlist.
+ 		* @param playlistURI Identifier for the playlist.
+ 		* @param playlistCoverURI Identifier for the playlist cover.
+ 		* @param hasLocation If the playlist has a location associated with it.
+ 		*/
         playlist(QString playlistName, QString playlistURL, QString playlistCoverURI, bool hasLocation);
+      
+      	/**
+ 		* @brief Gets a playlist name.
+ 		* 
+ 		* @return A playlist name
+ 		*/
         QString getPlaylistName();
+      
+      	/**
+ 		* @brief Gets a playlist identifier.
+ 		* 
+ 		* @return A playlist identifier.
+ 		*/
         QString getPlaylistURI();
+      
+      	/**
+ 		* @brief Gets a playlist cover identifier.
+ 		* 
+ 		* @return A playlist cover identifier.
+ 		*/
         QString getPlaylistCoverURI();
 
+      	/**
+ 		* @brief Sets a playlist name.
+ 		* 
+ 		* @param newName The new playlist name.
+ 		*/
         void setPlaylistName(QString newName);
+      
+      	/**
+ 		* @brief Sets a playlist identifier.
+ 		* 
+ 		* @param newURI The new playlist identifier.
+ 		*/
         void setPlaylistURI(QString songURI);
+      
+      	/**
+ 		* @brief Sets a playlist cover identifier.
+ 		* 
+ 		* @param newCoverURI The new playlist cover identifier.
+ 		*/
         void setPlaylistCoverURI(QString songArtist);
-        // bool hasSong(QString songURI);
-        // bool isPlaying(QString songURI);
-        // Song::song getSong(QString songURI);
     };
 
     

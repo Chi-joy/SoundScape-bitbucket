b3b93f4245b9fe8b480b4e44ea660b0165234110
Valerie Lozano-Ramos <vlozanor@uwo.ca>
2023-04-04 01:18:30 +0000

    doxy

--- shubh/SoundScape/PlaylistMap.h
+++ shubh/SoundScape/PlaylistMap.h
@@ -3,27 +3,55 @@
 #include <QQmlEngine>
 
 #include "playlist.h"
+
+/**
+ * @brief Represents a mapping between a location and a playlist.
+ */
 class PlaylistMap {
 private:
-    location::Location location;
-    Playlist::playlist playlist;
+    location::Location location; /**< The location associated with this PlaylistMap. */
+    Playlist::playlist playlist; /**< The playlist associated with this PlaylistMap. */
 public:
     //static std::set<PlaylistMap> PlaylistMaps;
 
+    /**
+     * @brief Constructs a new PlaylistMap object with the given location and playlist.
+     *
+     * @param loc The location to be associated with this PlaylistMap.
+     * @param pl The playlist to be associated with this PlaylistMap.
+     */
     PlaylistMap(location::Location loc, Playlist::playlist pl) :location(loc), playlist(pl) {
 
         location = loc;
         playlist = pl;
     }
 
+    /**
+     * @brief Updates the playlist for this PlaylistMap object.
+     *
+     * @param updatedPlaylist The updated playlist to be set for this PlaylistMap.
+     */
     void updatePlaylist(Playlist::playlist updatedPlaylist);
 
+    /**
+     * @brief Updates the location for this PlaylistMap object.
+     *
+     * @param updatedLocation The updated location to be set for this PlaylistMap.
+     */
     void updateLocation(location::Location updatedLocation);
 
+    /**
+     * @brief Returns the current playlist for this PlaylistMap object.
+     *
+     * @return The current playlist.
+     */
     Playlist::playlist getPlaylist();
 
+    /**
+     * @brief Returns the current location for this PlaylistMap object.
+     *
+     * @return The current location.
+     */
     location::Location getLocation();
-
-
-
 };
+

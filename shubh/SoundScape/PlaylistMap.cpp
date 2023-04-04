de15f8163298730e698b7859334f0ba98f4764d9
Valerie Lozano-Ramos <vlozanor@uwo.ca>
2023-04-04 01:16:21 +0000

    doxy

--- shubh/SoundScape/PlaylistMap.cpp
+++ shubh/SoundScape/PlaylistMap.cpp
@@ -1,17 +1,37 @@
 #include "PlaylistMap.h"
 
+/**
+ * @brief Updates the playlist for this PlaylistMap object.
+ *
+ * @param updatedPlaylist The updated playlist to be set for this PlaylistMap.
+ */
 void PlaylistMap::updatePlaylist(Playlist::playlist updatedPlaylist){
     this->playlist = updatedPlaylist;
 }
         
+/**
+ * @brief Updates the location for this PlaylistMap object.
+ *
+ * @param updatedLocation The updated location to be set for this PlaylistMap.
+ */
 void PlaylistMap::updateLocation(location::Location updatedLocation){
     this-> location = updatedLocation;
 }
 
+/**
+ * @brief Returns the current playlist for this PlaylistMap object.
+ *
+ * @return The current playlist.
+ */
 Playlist::playlist PlaylistMap::getPlaylist(){
     return this->playlist;
 }
         
+/**
+ * @brief Returns the current location for this PlaylistMap object.
+ *
+ * @return The current location.
+ */
 location::Location PlaylistMap::getLocation(){
     return this->location;
 }
@@ -23,4 +43,3 @@ location::Location PlaylistMap::getLocation(){
 
 
 
-// std::set<PlaylistMap> PlaylistMap::PlaylistMaps;

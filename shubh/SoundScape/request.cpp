9fdeadd388f1fb6faf9c3654a0f33b1d4c57990a
Namashivayan Sivaram <nsivaram@uwo.ca>
2023-04-04 01:13:59 +0000

    request.cpp edited online with Bitbucket

--- shubh/SoundScape/request.cpp
+++ shubh/SoundScape/request.cpp
@@ -17,11 +17,19 @@ Assignment 1
 
 #include <QNetworkRequest>
 #include <QNetworkAccessManager>
+/**
+ * @file
+ * @brief This file contains the implementation of a program that interacts with the Spotify Web API to retrieve playlists.
+ */
 #include <QNetworkReply>
 #include "request.h"
 
 using namespace std;
 
+/**
+ * @struct MemoryStruct
+ * @brief A structure used for storing memory while downloading data from the Spotify API.
+ */
 struct MemoryStruct
 {
     char* memory;
@@ -31,6 +39,15 @@ struct MemoryStruct
 //callback function for curl request
 std::string theAccessToken;
 static size_t
+/**
+ * @brief A callback function used by the cURL library to store the data received from the Spotify API.
+ * 
+ * @param contents The received data.
+ * @param size The size of each data element.
+ * @param nmemb The number of data elements.
+ * @param userp A pointer to a struct MemoryStruct object.
+ * @return The total size of the data received (in bytes).
+ */
 WriteMemoryCallback(void* contents, size_t size, size_t nmemb, void* userp)
 {
     size_t realsize = size * nmemb;
@@ -58,19 +75,32 @@ WriteMemoryCallback(void* contents, size_t size, size_t nmemb, void* userp)
     free(ptr);
     return realsize;
 }
-
+/**
+ * @brief A function used to set the access token retrieved from the Spotify API.
+ * 
+ * @param myjson A nlohmann::json object containing the access token data.
+ */
 void setToken(nlohmann::json myjson) {
 
     //initRequest(toSend);
     //accessToken = myjson["access_token"];
 
 }
-
+/**
+ * @brief A function used to send a request to the Spotify API and retrieve a user's playlists.
+ * 
+ * @param accessToken The access token used to authenticate the request.
+ */
 void getPlaylists(std::string accessToken)
 {
 
 }
-
+/**
+ * @brief A function used to extract the playlist data from a JSON object returned by the Spotify API.
+ * 
+ * @param myjson A reference to a nlohmann::json object containing the playlist data.
+ * @return A vector containing Playlist objects, each representing a playlist.
+ */
 vector<Playlist::playlist> createVector(nlohmann::json & myjson) {
     vector<Playlist::playlist> vPlaylist;
 
@@ -112,6 +142,12 @@ vector<Playlist::playlist> createVector(nlohmann::json & myjson) {
 }
 
 //make curl request
+/**
+ * @brief A function used to send a request to the Spotify API and retrieve an access token.
+ * 
+ * @param authCode The authorization code used to request the access token.
+ * @return A QString containing the access token.
+ */
 QString initRequest(std::string authCode)
 {
     CURL* curl_handle;
